78e2e3b16ac3c46d19f0af1d96bb51a853defa7d
cindylevy7820 <132998034+cindylevy7820@users.noreply.github.com>
2023-05-23 17:13:28 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+
+# Charger le modèle
+pickle_in = open('model/LGBMClassifier.pkl', 'rb')
+model = pickle.load(pickle_in)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    # Convertir les données en dataframe
+    df = pd.DataFrame(data)
+    # Prétraitement des données si nécessaire
+    
+    # Effectuer la prédiction
+    predictions = model.predict_proba(df)[:, 1]
+    
+    # Préparer la réponse
+    response = {'predictions': predictions.tolist()}
+    
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(port=5001)
