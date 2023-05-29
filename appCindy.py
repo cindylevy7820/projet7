7826e64c7e37719c3078e7a9c86769cd544fe51d
cindylevy7820 <132998034+cindylevy7820@users.noreply.github.com>
2023-05-29 19:29:16 +0200

    Update appCindy.py

--- appCindy.py
+++ appCindy.py
@@ -50,7 +50,7 @@ def load_predictions_API(sample, id, clf):
     X = sample.iloc[:, :-1]
     data = X[X.index == int(id)].to_dict(orient='records')
     print(data)
-    response = requests.post('https://cindylevy7820-projet7-appcindy-vmvj8w.streamlit.app', json=data)    
+    response = requests.post('https://cindylevy7820-projet7-appcindy-vmvj8w.streamlit.app', json=data[0)    
     predictions = response.json()['predictions']
     return predictions[0]
 
